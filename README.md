dff7f6f0819c0d8fceca74e5d11e6ac6cf778c6d
Vishal Ramteke <vramtekegcp@gmail.com>
2018-07-09 21:22:15 +1000

    Add functionality to start and stop EC2 instances

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ Purpose
 =======
 These are scripts to manage and maintain AWS infrastructure.
 
+Assumptions:
+============
+Project tag is used to tag all resources.If not, the script would return all resources by default.
+
 Pre-reqs:
 ========
 
@@ -14,6 +18,7 @@ Pre-reqs:
 How to execute the script :
 ===========================
 $ pipenv run python awsmm.py
-$ pipenv run python awsmm.py --project=<Project tag> ## to list instances belonging to a parituclar project
+$ pipenv run python awsmm.py <command> --project=<Project tag> 
+  where <command> is either list, start or stop and --project is optional.
 
 
--- awsmm.py
+++ awsmm.py
@@ -4,17 +4,25 @@ import click
 session = boto3.Session()
 ec2 = session.resource('ec2')
 
-@click.command()
-@click.option('--project', default=None)
-
-def list_instances(project):
-  "list-instances : To list all instances in the AWS account"
+def filter_instances(project):
   instances= [] #Initialise empty list to process projectName parameter
   if project:
-      filters= [{'Name':'tag:Project','Values':[project]}]
-      instances = ec2.instances.filter(Filters=filters)
+     filters= [{'Name':'tag:Project','Values':[project]}]
+     instances = ec2.instances.filter(Filters=filters)
   else :
-      instances = ec2.instances.all()
+     instances = ec2.instances.all()
+  return instances
+
+
+@click.group()
+def instances ():
+    "Commands for instances"
+
+@instances.command('list')
+@click.option('--project', default=None)
+def list_instances(project):
+  "List EC2 instances in the AWS account "
+  instances = filter_instances(project)
 
   for i in instances:
     tag_names={t['Key']:t['Value'] for t in i.tags or []}
@@ -29,5 +37,31 @@ def list_instances(project):
     )))
   return
 
+
+@instances.command('start')
+@click.option('--project', default=None)
+def start_instances(project):
+  "Start EC2 instances in the AWS account"
+  instances = filter_instances(project)
+
+  for i in instances:
+      print('Starting instance {0} ...'.format(i.id))
+      i.start()
+
+  return
+
+
+@instances.command('stop')
+@click.option('--project', default=None)
+def stop_instances(project):
+  "Stop EC2 instances in the AWS account"
+  instances = filter_instances(project)
+
+  for i in instances:
+      print('Stopping instance {0} ...'.format(i.id))
+      i.stop()
+
+  return
+
 if __name__ == '__main__':
-  list_instances()
+  instances()
